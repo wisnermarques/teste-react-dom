dc3fc4ec083b26ef46862f41aa2acb8c9080dff8
Wisner Marques <wisner.a.marques@gmail.com>
2023-09-01 10:34:42 -0300

    loading

---
+++ src/components/Loading.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Loading.css' // Importe os estilos CSS
+
+function Loading() {
+  return (
+    <div className='loading_container'>
+      <div className='loading_spinner'></div>
+    </div>
+  )
+}
+
+export default Loading
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -3,33 +3,38 @@ import ExibirTarefas from '../components/ExibirTarefas'
 
 import tarefaService from '../services/tarefas'
 import Botao from '../components/Botao'
+import Loading from '../components/Loading'
 
 function Home() {
   const [tarefas, setTarefas] = useState([])
   const [tarefa, setTarefa] = useState('')
   const [erro, setErro] = useState(null)
+  const [isLoading, setIsLoading] = useState(true)
 
   useEffect(() => {
-    tarefaService
-      .getAll()
-      .then((response) => {
-        setTarefas(response.data)
-      })
-      .catch((error) => {
-        if (error.response) {
-          // O servidor respondeu com um status de erro
-          console.error('Erro na requisição:', error.response)
-        } else if (error.request) {
-          // A requisição foi feita, mas não houve resposta do servidor
-          console.error('Não foi possível se conectar ao servidor.')
-          setErro(
-            'Não foi possível se conectar ao servidor. Verifique sua conexão de rede.'
-          )
-        } else {
-          // Algo aconteceu na configuração da requisição que causou o erro
-          console.error('Erro na configuração da requisição:', error.message)
-        }
-      })
+    setTimeout(() => {
+      tarefaService
+        .getAll()
+        .then((response) => {
+          setTarefas(response.data)
+          setIsLoading(false)
+        })
+        .catch((error) => {
+          if (error.response) {
+            // O servidor respondeu com um status de erro
+            console.error('Erro na requisição:', error.response)
+          } else if (error.request) {
+            // A requisição foi feita, mas não houve resposta do servidor
+            console.error('Não foi possível se conectar ao servidor.')
+            setErro(
+              'Não foi possível se conectar ao servidor. Verifique sua conexão de rede.'
+            )
+          } else {
+            // Algo aconteceu na configuração da requisição que causou o erro
+            console.error('Erro na configuração da requisição:', error.message)
+          }
+        })
+    }, 5000)
   }, [])
 
   const addTarefa = (event) => {
@@ -56,25 +61,37 @@ function Home() {
         <p>{erro}</p>
       ) : (
         <>
-          <h2>Lista de tarefas:</h2>
-          <ExibirTarefas tarefas={tarefas} setTarefas={setTarefas} />
-          <hr />
-          <h2>Adicionar tarefa:</h2>
-          <form onSubmit={addTarefa}>
-            <div className='mb-3'>
-              <label htmlFor='tarefa' className='form-label'>
-                Tarefa:
-              </label>
-              <input
-                type='text'
-                placeholder='Digite a tarefa...'
-                className='form-control'
-                value={tarefa}
-                onChange={handleTarefaChange}
-              />
-            </div>
-            <Botao text='Adicionar' />
-          </form>
+          {isLoading ? (
+            <Loading />
+          ) : (
+            <>
+              <h2>Lista de tarefas:</h2>
+              {tarefas.length > 0 ? (
+                <ExibirTarefas tarefas={tarefas} setTarefas={setTarefas} />
+              ) : (
+                <p className='alert alert-warning' role='alert'>
+                  Não existem tarefas cadastradas!
+                </p>
+              )}
+              <hr />
+              <h2>Adicionar tarefa:</h2>
+              <form onSubmit={addTarefa}>
+                <div className='mb-3'>
+                  <label htmlFor='tarefa' className='form-label'>
+                    Tarefa:
+                  </label>
+                  <input
+                    type='text'
+                    placeholder='Digite a tarefa...'
+                    className='form-control'
+                    value={tarefa}
+                    onChange={handleTarefaChange}
+                  />
+                </div>
+                <Botao text='Adicionar' />
+              </form>
+            </>
+          )}
         </>
       )}
     </div>
